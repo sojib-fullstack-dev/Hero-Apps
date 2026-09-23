b1cadd38f9bf93901875d221d248759cbaa6e0ef
samiulhasansojibe <samiulhasansojib069@gmail.comm>
2026-09-23 21:47:03 +0600

    Almost Done

--- src/app/apps/page.tsx
+++ src/app/apps/page.tsx
@@ -24,13 +24,13 @@ const AppsPages = async () => {
             {/* data card  */}
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
-                {
+                {   data?
                     data.map((app: TApp, ind: number) => {
                         return (
                             <AppCard key={ind} app={app} />
                         )
 
-                    })
+                    }):<p>No Data Found</p>
                 }
 
             </div>
