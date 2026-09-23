f67ce28f1567565fe163641283b14ab6173ef5f1
samiulhasansojibe <samiulhasansojib069@gmail.comm>
2026-09-23 21:44:33 +0600

    Almost Done

--- src/lib/app.ts
+++ src/lib/app.ts
@@ -1,5 +1,7 @@
  export const getData = async () => {
     const res = await fetch('https://hero-apps-sage.vercel.app/data.json');
     const data = await res.json();
-    return data;
+    
+   return data ? data:[]
+    
 }
